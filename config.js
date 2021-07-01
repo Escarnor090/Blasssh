f68cb2d1c9c3f501f0610337027cb1288ddab2cb
Escarnor090 <86784922+Escarnor090@users.noreply.github.com>
2021-07-01 20:21:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Các bạn uis',
+    introDesc: `Tui muốn hỏi mấy bạn ...`,
+    btnIntro: 'Gì ?',
+    title: 'Tui có đẹp trai hông?',
+    desc: 'Đẹp trai nhất thế giới? ',
+    btnYes: 'Có bạn sang đẹp trai vcl ><',
+    btnNo: 'Không, bạn à :))',
+    question:'Tại sao mấy bạn thấy tui đẹp dị?',
+    btnReply: 'For sang',
+    reply: 'YBởi vì tui có cặp mắt say đắm lòng người :)))',
+    mess: 'Tui biết mà :))',
+    messDesc: 'Chúc mấy bạn thi tốt nhan <3 ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
